a638d04d37e5216eb127090385c76d3fb74f22de
Buezman <54446003+Buezman@users.noreply.github.com>
2022-07-07 09:47:49 +0100

    abstracted db credentials

--- src/main/java/com/buezman/springtransactiondemo/controller/BankController.java
+++ src/main/java/com/buezman/springtransactiondemo/controller/BankController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
